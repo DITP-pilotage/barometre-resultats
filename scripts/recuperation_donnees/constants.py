90be65805759b9b7eddc153d616f0c58a6a1ef6a
Colin Dugeai <colin.dugeai@gmail.com>
2024-02-07 16:46:05 +0100

    minor

--- scripts/recuperation_donnees/constants.py
+++ scripts/recuperation_donnees/constants.py
@@ -1,4 +1,4 @@
 BASE_URL_API="https://api.github.com/repos/DITP-pilotage/barometre-resultats/contents/data"
 
+GITHUB_BRANCH_ALLOWED=['prod', 'preprod']
 GITHUB_BRANCH_DEFAULT='prod'
-GITHUB_BRANCH_ALLOWED=[GITHUB_BRANCH_DEFAULT, 'preprod']
