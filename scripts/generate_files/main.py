0600f0106c2a37a77bba3f6f544ef60c37c3cf74
Colin Dugeai <colin.dugeai@gmail.com>
2024-04-15 12:14:50 +0200

    meta_indicateurs: nouvelle table
    Ajout de la table des metadonnees indicateurs

--- scripts/generate_files/main.py
+++ scripts/generate_files/main.py
@@ -9,10 +9,13 @@ if __name__ =="__main__":
 
     # Récupération des données du baro
     donnees_baro = utils.get_data("Base cible", os.environ.get('PG_URL'))
+    metadata_baro = utils.get_metadata("Base cible", os.environ.get('PG_URL'))
     #donnees_baro_dev = utils.get_data("Base de dev", os.environ.get('PG_URL_DEV'))
     # Export des données du baro dans un fichier séparé par indicateur
     utils.wipe_dir('../../data/')
     utils.export_by_indic(donnees_baro, "../../data/")
+    utils.wipe_dir('../../metadata/')
+    metadata_baro.to_csv("../../metadata/meta_indicateurs.csv", index=False)
     #utils.export_by_indic(donnees_baro_dev, "out_dev/")
 
     print('done')
--- scripts/generate_files/utils.py
+++ scripts/generate_files/utils.py
@@ -47,6 +47,16 @@ def get_data(connection_alias, db_url):
 
     return res
 
+def get_metadata(connection_alias, db_url):
+    QUERY = "SELECT * FROM barometre.baro_meta_indicateurs"
+
+    db = DatabaseHelper(connection_alias, db_url)
+    db.connect()
+    res = db.query(QUERY)
+    db.disconnect()
+
+    return res
+
 def export_by_indic(data_, out_dir):
 
     n_files_exported=0
