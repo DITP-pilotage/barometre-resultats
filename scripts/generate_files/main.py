79566c31610b52f86225a4eb54ae6886362822eb
Colin Dugeai <colin.dugeai@gmail.com>
2024-04-26 12:02:04 +0200

    refacto: clean script

--- scripts/generate_files/main.py
+++ scripts/generate_files/main.py
@@ -7,15 +7,16 @@ if __name__ =="__main__":
     
     load_dotenv()
 
-    # Récupération des données du baro
+    # 1- Données quanti des indicateurs
     donnees_baro = utils.get_data("Base cible", os.environ.get('PG_URL'))
-    metadata_baro = utils.get_metadata("Base cible", os.environ.get('PG_URL'))
-    #donnees_baro_dev = utils.get_data("Base de dev", os.environ.get('PG_URL_DEV'))
-    # Export des données du baro dans un fichier séparé par indicateur
     utils.wipe_dir('../../data/')
+    ##  Export des données dans un fichier séparé par indicateur
     utils.export_by_indic(donnees_baro, "../../data/")
+
+    # 2- Données quali des indicateurs
+    metadata_baro = utils.get_metadata("Base cible", os.environ.get('PG_URL'))
     utils.wipe_dir('../../metadata/')
+    ##  Export des données dans metadata/meta_indicateurs.csv
     metadata_baro.to_csv("../../metadata/meta_indicateurs.csv", index=False)
-    #utils.export_by_indic(donnees_baro_dev, "out_dev/")
 
     print('done')
