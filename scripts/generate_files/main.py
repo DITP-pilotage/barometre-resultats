45c1db5b89339264524e4f5af5d81bf37b697f11
Colin Dugeai <colin.dugeai@gmail.com>
2024-05-14 11:25:01 +0200

    tmp: disable meta refresh

--- scripts/generate_files/main.py
+++ scripts/generate_files/main.py
@@ -14,11 +14,6 @@ if __name__ =="__main__":
     ##  Export des données dans un fichier séparé par indicateur
     utils.export_by_indic(donnees_baro, "../../data/")
 
-    # 2- Données quali des indicateurs
-    metadata_baro = utils.get_metadata("Base cible", os.environ.get('PG_URL'))
-    utils.wipe_dir('../../metadata/')
-    ##  Export des données dans metadata/meta_indicateurs.csv
-    metadata_baro.to_csv("../../metadata/meta_indicateurs.csv", index=False)
 
     # Si GIT_DO_PUSH=true, on exécute les commandes git
     if os.environ.get('GIT_DO_PUSH').upper()=='TRUE':
