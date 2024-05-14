95ee6154f07a61b841d2633df7dac9ec92a6d47c
Colin Dugeai <colin.dugeai@gmail.com>
2024-05-14 10:35:27 +0200

    fix: get date on commit time
    not on modulea loading

--- scripts/generate_files/utils_git.py
+++ scripts/generate_files/utils_git.py
@@ -9,13 +9,14 @@ def run_command(cmd_to_run):
 GIT_REPO_URL="github.com/DITP-pilotage/barometre-resultats.git"
 GIT_BOT_USERNAME="bot-data"
 GIT_BOT_EMAIL="bot-data@ditp.fr"
-GIT_COMMIT_MSG="data::"+datetime.now().strftime("%d-%m-%Y")
 
 # Credentials for pushing
 GIT_USER=os.environ.get('GIT_USER')
 GIT_TOKEN=os.environ.get('GIT_TOKEN')
 
 def git_add_commit_push():
+    GIT_COMMIT_MSG="data::"+datetime.now().strftime("%d-%m-%Y")
+
     run_command('git add ../../data/')
     run_command('git add ../../metadata/')
     # Commit avec un auteur spécifique: GIT_BOT_USERNAME
