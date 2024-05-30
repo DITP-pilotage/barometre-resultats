2f8b3c0fdc97c3109108f1d2c585e7b611e53f3e
Colin Dugeai <46027833+cdugeai@users.noreply.github.com>
2024-05-30 15:16:15 +0200

    Nouvelles vues: meta_ch, meta_engag (PIL-183)
    * feat: add meta_chantiers (PIL-183)
    
    * feat: add meta_engagement (PIL-183)
    
    * fix: typo
    
    * data::29-05-2024
    
    * Update config-viz.yml
    
    msg CYSO
    
    * data::29-05-2024
    
    * data::30-05-2024
    
    * feat: add meta_chantiers (PIL-183)
    
    * feat: add meta_engagement (PIL-183)
    
    * fix: typo
    
    ---------
    
    Co-authored-by: bot-data <bot-data@ditp.fr>

--- scripts/generate_files/main.py
+++ scripts/generate_files/main.py
@@ -8,17 +8,28 @@ if __name__ =="__main__":
     
     load_dotenv()
 
+    db_url = os.environ.get('PG_URL')
+
     # 1- Données quanti des indicateurs
-    donnees_baro = utils.get_data("Base cible", os.environ.get('PG_URL'))
+    donnees_baro = utils.get_data("Base cible", db_url)
     utils.wipe_dir('../../data/')
     ##  Export des données dans un fichier séparé par indicateur
     utils.export_by_indic(donnees_baro, "../../data/")
 
-    # 2- Données quali des indicateurs
-    metadata_baro = utils.get_metadata("Base cible", os.environ.get('PG_URL'))
+    # 2- Données quali
+
     utils.wipe_dir('../../metadata/')
-    ##  Export des données dans metadata/meta_indicateurs.csv
-    metadata_baro.to_csv("../../metadata/meta_indicateurs.csv", index=False)
+    # 2.1- Metadonnées indicateurs
+    baro_meta_indicateurs = utils.get_metadata("Base cible", db_url)
+    baro_meta_indicateurs.to_csv("../../metadata/meta_indicateurs.csv", index=False)
+
+    # 2.2- Metadonnées chantiers
+    baro_meta_chantiers = utils.get_metadata_ch("Base cible", db_url)
+    baro_meta_chantiers.to_csv("../../metadata/meta_chantiers.csv", index=False)
+
+    # 2.3- Metadonnées engagement
+    baro_meta_engagement = utils.get_metadata_engagement("Base cible", db_url)
+    baro_meta_engagement.to_csv("../../metadata/meta_engagement.csv", index=False)
 
     # Si GIT_DO_PUSH=true, on exécute les commandes git
     if os.environ.get('GIT_DO_PUSH', '').upper()=='TRUE':
